0b729891958206eab3b17370210cad71e0cd5372
Manjusha Chava <manjushachava@gmail.com>
2017-10-15 22:30:56 -0400

    Exercise 4 done.

---
+++ Divisors.py
@@ -0,0 +1,11 @@
+#Program 4
+
+# Check if user input is even or odd
+
+a = range(2, 11)
+
+number = int(input("Enter a random number: "))
+
+for element in a:
+    if number % element == 0:
+        print(str(number) + " is divisible by " + str(element))
