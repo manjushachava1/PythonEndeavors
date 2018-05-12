fc140f3e9d0dac20b3c60d6e8a9a58bd08e71c73
Manjusha Chava <manjushachava@gmail.com>
2018-05-11 20:44:51 -0400

    Exercise 5 done.

--- ListLessThanTen.py
+++ ListLessThanTen.py
@@ -1,7 +1,7 @@
 #Program 3
 
-#Take a list and print out the numbers less than 5
-#Extras:
+# Take a list and print out the numbers less than 5
+# Extras:
 # 1. Write program in one line of code
 # 2. Ask the user for a number and return a list that contains elements that are smaller than that number given by the user.
 
@@ -9,13 +9,13 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = []
 c = []
 
-#Extra 1
+# Extra 1
 for element in a:
     if element < 5:
         b.append(element)
 print(b)
 
-#Extra 2
+# Extra 2
 num = int(input("Enter a random number: "))
 for element in a:
     if element < num:
---
+++ ListOverlap.py
@@ -0,0 +1,26 @@
+# Program 3
+
+# Take two lists and write a program that returns a list that only contains the elements that are common (without duplicates)
+# Extras:
+# 1. Randomly generate two lists to test this
+# 2. Write this in one line of Python
+
+import random
+
+a = random.sample(range(30), 5)
+b = random.sample(range(10), 5)
+c = []
+
+for elem in a:
+    if elem in b:
+        c.append(elem)
+
+print(a, b, c)
+
+fin = list(set(c))
+
+print(fin)
+
+# Extra 2
+print(set(a) & set(b))
+
